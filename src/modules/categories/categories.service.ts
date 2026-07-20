6625f214d534a51a6698890ff3f66cda2cd5b00e
torufa <torufa3@gmail.com>
2026-07-21 00:34:48 +0600

    fix: prevent duplicate category creation

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,6 +1,12 @@
 import { prisma } from "../../lib/prisma";
 
 const createCategoriesIntoDB = async(categoryName: string) => {    
+    const isCategoryExists = await prisma.categories.findUniqueOrThrow({
+        where: {categoryName}
+    })
+    if(isCategoryExists){
+        throw new Error("Category already exists.")
+    }
 
     const result = await prisma.categories.create({
         data: {

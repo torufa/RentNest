09531f9161f6eb7835749e8877025f380852d281
torufa <torufa3@gmail.com>
2026-07-22 21:54:48 +0600

    fix: update getAllUser api endpoint with auth middleware(Admin)

--- src/modules/admin/admin.route.ts
+++ src/modules/admin/admin.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { adminController } from "./admin.controller";
+import { UserRole } from "../../../generated/prisma/enums";
+import { auth } from "../../middleware/auth";
 
 const router = Router()
 
-router.get("/users", adminController.getAllUser)
+router.get("/users", auth(UserRole.ADMIN), adminController.getAllUser)
 
 export const adminRoutes = router;
